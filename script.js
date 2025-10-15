b5f76430406b619ab67b21efaeb887a8a28741c0
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-10-15 01:24:00 +0100

    Gsap js animations implementation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
 import Lenis from "lenis";
+import { start } from "repl";
 
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
@@ -14,4 +15,72 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const features = document.querySelectorAll(".feature");
     const featureBgs = document.querySelectorAll(".featureBg");
-})
+
+    const featureStartPosition = [
+        {top: 25, left: 15},
+        {top: 12.5, left: 50},
+        {top: 22.5, left: 75},
+        {top: 30, left: 82.5},
+        {top: 50, left: 20},
+        {top: 80, left: 20},
+        {top: 75, left: 75},
+    ];
+
+    features.forEach((feature, index) => {
+        const featurePos = featureStartPosition[index];
+        gsap.set(feature, {
+            top: `${featurePos.top}%`,
+            left: `${featurePos.left}%`,
+        });
+    });
+
+    const featureStartDimensions = [];
+
+    featureBgs.forEach((bg) => {
+        const rect = bg.getBoundingClientRect();
+        featureStartDimensions.push({
+            width: rect.width,
+            height: rect.height,
+        });
+    });
+
+    const remInPixels = parseFloat(
+        getComputedStyle(document.documentElement).fontSize
+    );
+    const targetWidth = 3 * remInPixels;
+    const targetHeight = 3 * remInPixels;
+
+    const getSearchBarFinalWidth = () => {
+        return window.innerWidth < 1000 ? 20 : 25;
+    }
+
+    let searchBarFinalWidth = getSearchBarFinalWidth();
+
+    window.addEventListener("resize", () => {
+        searchBarFinalWidth = getSearchBarFinalWidth();
+        ScrollTrigger.refresh();
+    });
+
+    ScrollTrigger.create({
+        trigger: ".spotlight",
+        start: "start",
+        end: `+=${window.innerHeight * 3}px`,
+        pin: true,
+        pinSpacing: true,
+        scrub: 1,
+        onUpdate: (self) => {
+            const progress = self.progress;
+
+            if (progress <= 0.3333){
+                const spotlightHeaderProgress = progress / 0.3333;
+                gsap.set(".spotlightContent", {
+                    y: `${-100 * spotlightHeaderProgress}%`
+                });
+            } else{
+                gsap.set(".spotlightContent", {
+                    y: "-100%",
+                });
+            }
+        }
+    });
+});
